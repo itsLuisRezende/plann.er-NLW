c6ac394f47c630db7f78ad90436521e9341a52f8
itsLuisRezende <140192412+itsLuisRezende@users.noreply.github.com>
2024-09-13 15:33:12 -0300

    atualizando pagina trip details e criando aba de salvar atividades

--- src/pages/create-trip/confirm-trip-modal.tsx
+++ src/pages/create-trip/confirm-trip-modal.tsx
@@ -21,25 +21,25 @@ export function ConfirmTripModal({
                         </button>
                     </div>
                     <p className="text-sm text-zinc-400">
-                        Para concluir a criação da viagem para <span className="font-semibold text-zinc-100">Florianópolis, Brasil</span> nas datas de <span className="font-semibold text-zinc-100">16 a 27 de Agosto de 2024</span> preencha seus dados abaixo:
+                        Para concluir a criação da viagem para <span className="font-semibold text-zinc-100">Florianópolis, Brasil
+                        </span> nas datas de <span className="font-semibold text-zinc-100">16 a 27 de Agosto de 2024</span> preencha seus dados abaixo:
                     </p>
-
-                    <form onSubmit={createTrip} className="space-y-3">
-                        <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-                            <User className="text-zinc-400 size-5"/>
-                            <input name="name" placeholder="Seu nome completo" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none flex-1"/>
-                        </div>
-
-                        <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-                            <User className="text-zinc-400 size-5"/>
-                            <input type="email" name="email" placeholder="Seu e-mail pessoal" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none flex-1"/>
-                        </div>
-
-                        <button type="submit" className="bg-lime-300 w-full justify-center text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400">
-                        Confirmar criação da viagem
-                        </button>
-                    </form>
                 </div>
+                <form onSubmit={createTrip} className="space-y-3">
+                    <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                        <User className="text-zinc-400 size-5"/>
+                        <input name="name" placeholder="Seu nome completo" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none flex-1"/>
+                    </div>
+
+                    <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                        <User className="text-zinc-400 size-5"/>
+                        <input type="email" name="email" placeholder="Seu e-mail pessoal" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none flex-1"/>
+                    </div>
+
+                    <button type="submit" className="bg-lime-300 w-full justify-center text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400">
+                        Confirmar criação da viagem
+                    </button>
+                </form>
             </div>
         </div>
     )
--- src/pages/create-trip/trip-details/index.tsx
+++ src/pages/create-trip/trip-details/index.tsx
@@ -1,6 +1,17 @@
-import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from "lucide-react";
+import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, UserCog, X, Tag } from "lucide-react";
+import { useState } from "react"
 
 export function TripDetailsPage() {
+    const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] = useState(false)
+
+    function openCreateActivityModal() {
+        setIsCreateActivityModalOpen(true)
+    }
+
+    function closeCreateActivityModal() {
+        setIsCreateActivityModalOpen(false)
+    }
+    
     return (
         <div className="max-w-6x1 px-6 py-10 mx-auto space-y-8">
             <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
@@ -24,11 +35,11 @@ export function TripDetailsPage() {
                 </div>
             </div>
 
-            <main className="flex gap-16">
+            <main className="flex gap-16 px-4">
                 <div className="flex-1 space-y-6">
                     <div className="flex items-center justify-between">
                         <h2 className="text-3xl font-semibold">Atividades</h2>
-                        <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                        <button onClick={openCreateActivityModal} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
                             <Plus className="size-5"/>
                             Cadastrar atividade
                         </button>
@@ -52,16 +63,118 @@ export function TripDetailsPage() {
                                 <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
                                     <CircleCheck className="size-5 text-lime-300" />
                                     <span className="text-zinc-100">Academia em grupo</span>
+                                    <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
+                                </div>
+                            </div>
+                            <div className="space-y-2.5">
+                                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                                    <CircleCheck className="size-5 text-lime-300" />
+                                    <span className="text-zinc-100">Academia em grupo</span>
+                                    <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="w-80">
-
+                <div className="w-80 space-y-6">
+                    <div className="space-y-6">
+                        <h2 className="font-semibold text-xl">Links importantes</h2>
+                        <div className="space-y-5">
+                            <div className="flex items-center justify-between gap-4">
+                                <div className="sapce-y-1.5">
+                                    <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
+                                    <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
+                                    https://www.airbnb.com.br/rooms/10470001123432423423432424
+                                    </a>
+                                </div>
+                                <Link2 className="text-zinc-400 size-5 shrink-0"/>
+                            </div>
+                            <div className="flex items-center justify-between gap-4">
+                                <div className="sapce-y-1.5">
+                                    <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
+                                    <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
+                                    https://www.airbnb.com.br/rooms/10470001123432423423432424
+                                    </a>
+                                </div>
+                                <Link2 className="text-zinc-400 size-5 shrink-0"/>
+                            </div>
+                        </div>
+                        <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700">
+                        <Plus className="size-5"/>
+                        Cadastrar novo link
+                        </button>
+                    </div>
+                    <div className="w-full h-px bg-zinc-800"/>
+                    <div className="space-y-6">
+                        <h2 className="font-semibold text-xl">Convidados</h2>
+                        <div className="space-y-5">
+                            <div className="flex items-center justify-between gap-4">
+                                <div className="sapce-y-1.5">
+                                    <span className="block font-medium text-zinc-100">Jessica White</span>
+                                    <span className="block text-sm text-zinc-400 truncate">
+                                        jessica.white44@yahoo.com
+                                    </span>
+                                </div>
+                                <CircleDashed className="text-zinc-400 size-5 shrink-0"/>
+                            </div>
+                            <div className="flex items-center justify-between gap-4">
+                                <div className="sapce-y-1.5">
+                                    <span className="block font-medium text-zinc-100">Dr. Rita Pacocha</span>
+                                    <span className="block text-sm text-zinc-400 truncate">
+                                        lacy.stiedenann@gmail.com
+                                    </span>
+                                </div>
+                                <CircleDashed className="text-zinc-400 size-5 shrink-0"/>
+                            </div>
+                        </div>
+                        <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700">
+                        <UserCog className="size-5"/>
+                        Gerenciar convidados
+                        </button>
+                    </div>
                 </div>
             </main>
+            
+            {isCreateActivityModalOpen && (
+                <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+                    <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+                        <div className="space-y-2">
+                            <div className="flex items-center justify-between">
+                                <h2 className="text-lg font-semibold">Cadastrar atividade</h2>
+                                <button type="button" onClick={closeCreateActivityModal}>
+                                <X className="size-5 text-zinc-400"/>
+                                </button>
+                            </div>
+                            <p className="text-sm text-zinc-400">
+                                Todos convidados podem visualizar as atividades.
+                            </p>
+                        </div>
+                        <form className="space-y-3">
+                            <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                                <Tag className="text-zinc-400 size-5"/>
+                                <input name="title"
+                                 placeholder="Qual a atividade?"
+                                 className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none flex-1"/>
+                            </div>
+
+                            <div className="flex items-center gap-2">
+                                <div className="h-14 flex-1 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                                    <Calendar className="text-zinc-400 size-5"/>
+                                    <input type="datetime-local"
+                                     name="occurs_at"
+                                     placeholder="Data e horário da atividade"
+                                     className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none flex-1"/>
+                                </div>
+                            </div>
+
+                            <button type="submit" className="bg-lime-300 w-full justify-center text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400">
+                                Salvar atividade
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            )}      
         </div>
     )
 }
